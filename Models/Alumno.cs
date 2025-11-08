60e170e8205edc578f15d59835df7950fd20f092
Moises Garcia <victorgpocas@gmail.com>
2025-11-08 00:47:12 -0600

    Creando las clase alumno y materia

---
+++ Models/Alumno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ColegioSanJose.Models
+{
+    public class Alumno
+    {
+        [Key]
+        public int AlumnoId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Apellido { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [StringLength(50)]
+        public string Grado { get; set; }
+
+        public ICollection<Expediente> Expedientes { get; set; }
+    }
+}
---
+++ Models/Materia.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using System.Collections.Generic;
+namespace ColegioSanJose.Models
+{
+    public class Materia
+    {
+        [Key]
+        public int MateriaId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string NombreMateria { get; set; }
+
+        [StringLength(100)]
+        public string Docente { get; set; }
+
+        public ICollection<Expediente> Expedientes { get; set; }
+    }
+}
