84f497dd32fc08e2eea4ed72e73d3207c9ed6c91
Moises Garcia <victorgpocas@gmail.com>
2025-11-08 01:08:18 -0600

    Creando los get y los set en una nueva clase model

---
+++ Models/AlumnoPromedioViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ColegioSanJose.Models
+{
+    public class AlumnoPromedioViewModel
+    {
+        public int AlumnoId { get; set; }
+        public string NombreCompleto { get; set; }
+        public decimal Promedio { get; set; }
+        public int CantMaterias { get; set; }
+    }
+}
