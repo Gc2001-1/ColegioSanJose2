361dfd227112f148ef987a730ae2c6c7991313e9
Moises Garcia <victorgpocas@gmail.com>
2025-11-08 00:48:38 -0600

    Creando la clase expediente

---
+++ Models/Expediente.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ColegioSanJose.Models
+{
+    public class Expediente
+    {
+        [Key]
+        public int ExpedienteId { get; set; }
+
+        [Required]
+        [ForeignKey("Alumno")]
+        public int AlumnoId { get; set; }
+        public Alumno Alumno { get; set; }
+
+        [Required]
+        [ForeignKey("Materia")]
+        public int MateriaId { get; set; }
+        public Materia Materia { get; set; }
+
+        [Range(0, 20)]
+        public decimal NotaFinal { get; set; }
+
+        [StringLength(500)]
+        public string Observaciones { get; set; }
+    }
+}
